72eebd69b22bdf9e51f0e4f5df1909e7684c27fc
IvanArjona <ivanarjonaalonso@gmail.com>
2022-02-16 20:38:27 +0100

    Smooth scroll

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,9 +2,9 @@ import { Html, Head, Main, NextScript, DocumentProps } from 'next/document'
 
 export default function MyDocument(props: DocumentProps) {
   return (
-    <Html>
+    <Html className="scroll-smooth">
       <Head />
-      <body className="bg-orange-500 dark:bg-black transition-colors duration-500">
+      <body className="bg-orange-500 dark:bg-black scroll-smooth transition-colors duration-500">
         <Main />
         <NextScript />
         <div id="modal-root"></div>
