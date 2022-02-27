11a071b2ff6cafd74b3490b9e57f7f3d49bd99d7
IvanArjona <ivanarjonaalonso@gmail.com>
2022-02-27 20:08:44 +0100

    Image unoptimized

--- components/presentation.tsx
+++ components/presentation.tsx
@@ -10,7 +10,7 @@ const Presentation: React.FC<{}> = () => {
     <div className="w-screen grid sm:grid-cols-2 gap-8">
       <div className="col-start-0 mx-auto">
         <div className="w-64 xl:w-72 2xl:w-80 shadow-[1rem_-1rem] shadow-purple-500">
-          <Image src={Photo} layout="intrinsic" />
+          <Image src={Photo} layout="intrinsic" unoptimized />
         </div>
       </div>
       <section className="-mt-20 sm:mt-auto z-10 sm:order-first sm:row-span-2 w-min mx-auto flex flex-col gap-2 md:gap-4 text-white">
