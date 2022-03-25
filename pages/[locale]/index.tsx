e027fa598afcb2663ad406e007c5bbd1363db7fc
IvanArjona <ivanarjonaalonso@gmail.com>
2022-03-25 20:34:50 +0100

    Page title and favicon

--- pages/[locale]/index.tsx
+++ pages/[locale]/index.tsx
@@ -1,5 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
+import { useTranslation } from 'next-i18next';
 import FullPage from '../../components/full-page';
 import Presentation from '../../components/presentation';
 import Experience from '../../components/experience';
@@ -12,6 +13,8 @@ import Projects from '../../components/projects';
 export { getStaticPaths, getStaticProps };
 
 const Home: NextPage = () => {
+  const { t } = useTranslation();
+
   const sections: React.ReactNode[] = [
     <FullPage key="presentation" nextSection="experience">
       <Presentation />
@@ -27,10 +30,12 @@ const Home: NextPage = () => {
     [prev, <Separator key={index} />, curr]
   ));
 
+  const title = `${process.env.fullName} - ${t('title-1')} ${t('title-2')}`;
+
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>{title}</title>
         <meta name="description" content="Description" />
       </Head>
       {sectionsWithSeparator}
