8a27dd2ed640a0a5530b8a59f3f228258f3d1b28
Iván Arjona Alonso <ivanarjonaalonso@gmail.com>
2023-10-12 20:29:49 +0200

    Add type to collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,12 +1,23 @@
 import { experienceSchema, projectsSchema, skillsSchema } from './_schemas';
 import { defineCollection } from 'astro:content';
 
-const experience = defineCollection({ schema: experienceSchema });
-const projects = defineCollection({ schema: projectsSchema });
-const skills = defineCollection({ schema: skillsSchema });
+const experienceCollection = defineCollection({
+  type: 'content',
+  schema: experienceSchema
+});
+
+const projectsCollection = defineCollection({
+  type: 'content',
+  schema: projectsSchema
+});
+
+const skillsCollection = defineCollection({
+  type: 'content',
+  schema: skillsSchema
+});
 
 export const collections = {
-  experience: experience,
-  projects: projects,
-  skills: skills,
+  experience: experienceCollection,
+  projects: projectsCollection,
+  skills: skillsCollection,
 };
