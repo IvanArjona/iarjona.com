7f6a19db7b7a20103963e068d973ea8f3c788076
Iván Arjona Alonso <ivanarjonaalonso@gmail.com>
2023-10-12 20:35:06 +0200

    Rename translatedCollection to filterCollectionByLanguage

--- src/utils/locale.ts
+++ src/utils/locale.ts
@@ -14,6 +14,6 @@ export async function changeLanguage(language?: string) {
   return i18next.changeLanguage(language || defaultLocale);
 }
 
-export function translatedCollection({ id }: { id: string }) {
+export function filterCollectionByLanguage({ id }: { id: string }) {
   return id.startsWith(getCurrentLanguage());
 }
